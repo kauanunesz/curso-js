0f81c1c6d39dfbac8f763599216733846894dd26
Kauã Nunes <kauanunes.dev09@gmail.com>
2025-11-09 14:20:13 -0300

    Operadores

---
+++ Operadores/script.js
@@ -0,0 +1,30 @@
+// Aula sobre operadores
+
+// Operadores aritméticos
+5 + 2 == 7;
+5 - 2 == 3;
+5 * 2 == 10;
+5 / 2 == 2.5;
+5 % 2 == 1;
+5 ** 2 == 25;
+
+// Incremento e decremento
+
+let n = 10;
+n += 5 == 15;
+n -= 10 == 5;
+n++ == 6;
+n-- == 5;
+
+// Relacionais
+//  >, <, >=, <=, !=, ==, ===, !==
+
+// Lógicos
+// !(Diferente), && (E), ||(Ou)
+
+// Operador Ternário
+
+// Condição ? verdadeiro : falso
+let num = 3
+num += 5 >= 7 ? 8 : 20;
+// Variável num vai receber um valor, com base na condição, se a condição der verdadeira, receberá + 8, se for falsa, receberá mais 20
